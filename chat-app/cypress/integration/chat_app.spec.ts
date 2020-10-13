974fa6ddd0e27a409fc17d096968037091fc2930
Ryan <ryan.michael.tate@gmail.com>
2020-10-12 19:10:56 -0700

    add chat e2e testyts test suite after running cypress front end test; check to ensure user and chat events exist.

--- chat-app/cypress/integration/chat_app.spec.ts
+++ chat-app/cypress/integration/chat_app.spec.ts
@@ -1,24 +1,60 @@
-describe('Test Fluvio Chat App', () => {
-    it('Should create a new user', async () => {
-        const faker = require('faker')
-        const name = faker.name.firstName().toLowerCase().replace(" ", "_");
-        const password = faker.internet.password().replace("_", "");
-        const text = faker.lorem.paragraphs() + '\n';
+describe("Test Fluvio Chat App", () => {
+  it("Should create a new user", async () => {
+    const faker = require("faker");
+    const user1 = "user" + (Math.random() * 1e4).toFixed();
+    const user2 = "user" + (Math.random() * 1e4).toFixed();
+    const password = faker.internet.password().replace("_", "");
+    const text1 = "TEST CHAT APP MESSAGE USER 1\n";
+    const text2 = "TEST CHAT APP MESSAGE USER 2\n";
 
-        cy.visit('/')
+    cy.visit("/registerUser").then(() => {
+      cy.get("#name").type(user1);
+      cy.get("#password").type(password);
 
-        cy.get('.jss286 > .jss10').click().then(() => {
-            cy.get('#name').type(name)
-            cy.get('#password').type(password)
+      cy.get(".MuiButtonBase-root")
+        .click()
+        .then(() => {
+          cy.get("#name").type(user1);
+          cy.get("#password").type(password);
 
-            cy.get('.MuiButtonBase-root').click().then(() => {
-                cy.get('#name').type(name)
-                cy.get('#password').type(password)
+          cy.get(".MuiButtonBase-root")
+            .click()
+            .then(async () => {
+              cy.get("#text").type(text1);
 
-                cy.get('.MuiButtonBase-root').click().then(async () => {
-                    cy.get('#text').type(text)
-                })
-            })
-        })
-    })
-})
+              cy.get(
+                ".MuiIconButton-edgeEnd > .MuiIconButton-label > .MuiSvgIcon-root"
+              ).click();
+
+              cy.get('.MuiList-root > [tabindex="0"]')
+                .click()
+                .then(async () => {
+                  cy.visit("/registerUser").then(() => {
+                    cy.get("#name").type(user2);
+                    cy.get("#password").type(password);
+
+                    cy.get(".MuiButtonBase-root")
+                      .click()
+                      .then(() => {
+                        cy.get("#name").type(user2);
+                        cy.get("#password").type(password);
+
+                        cy.get(".MuiButtonBase-root")
+                          .click()
+                          .then(async () => {
+                            cy.get("#text").type(text2);
+
+                            cy.get(
+                              ".MuiIconButton-edgeEnd > .MuiIconButton-label > .MuiSvgIcon-root"
+                            ).click();
+
+                            cy.get('.MuiList-root > [tabindex="0"]').click();
+                          });
+                      });
+                  });
+                });
+            });
+        });
+    });
+  });
+});
---
+++ chat-app/test/chat.spec.ts
@@ -0,0 +1,112 @@
+import {
+  TestSuite,
+  BeforeAll,
+  Test,
+  TestCase,
+  expect,
+  XTest,
+  Timeout,
+  AfterAll,
+} from "testyts";
+import Fluvio, {
+  FluvioAdmin,
+  OffsetFrom,
+  PartitionConsumer,
+} from "@fluvio/client";
+
+@TestSuite()
+export class FluvioChatAppE2ETests {
+  fluvio: Fluvio;
+  admin: FluvioAdmin;
+
+  userTopic = "nsc-user-events";
+  chatTopic = "nsc-chat-events";
+
+  usernamePrefix = "user";
+  text1 = "TEST CHAT APP MESSAGE USER 1";
+  text2 = "TEST CHAT APP MESSAGE USER 2";
+
+  userConsumer: PartitionConsumer;
+  chatConsumer: PartitionConsumer;
+
+  @BeforeAll()
+  @TestCase("Set Fluvio Client")
+  async beforeAll() {
+    // Set fluvio client for test suite;
+    this.fluvio = await Fluvio.connect();
+
+    expect.toBeEqual(this.fluvio instanceof Fluvio, true);
+
+    // // Set the admin for the test suite;
+    this.admin = await this.fluvio.admin();
+    expect.toBeEqual(this.admin instanceof FluvioAdmin, true);
+
+    this.userConsumer = await this.fluvio.partitionConsumer(this.userTopic, 0);
+    expect.toBeEqual(this.userConsumer instanceof PartitionConsumer, true);
+
+    this.chatConsumer = await this.fluvio.partitionConsumer(this.chatTopic, 0);
+    expect.toBeEqual(this.userConsumer instanceof PartitionConsumer, true);
+  }
+
+  @Test()
+  @TestCase("Find chat app topics")
+  public async findTopics() {
+    expect.not.toBeEqual(this.userTopic, undefined);
+    const userTopic = await this.admin.findTopic(this.userTopic);
+    expect.toBeEqual(userTopic.name, this.userTopic);
+
+    expect.not.toBeEqual(this.chatTopic, undefined);
+    const chatTopic = await this.admin.findTopic(this.chatTopic);
+    expect.toBeEqual(chatTopic.name, this.chatTopic);
+  }
+
+  @Test()
+  @TestCase("Fetch users")
+  public async fetchUsers() {
+    const {
+      records: { batches },
+    } = await this.userConsumer.fetch({
+      index: 8,
+      from: OffsetFrom.End,
+    });
+
+    const events: string[] = batches
+      .map(({ records }) => {
+        return records[0].value;
+      })
+      .filter((value) => {
+        return value.includes("Registered");
+      });
+
+    expect.toBeEqual(
+      events.length,
+      2,
+      `expected two user registration events, found: ${events.length}`
+    );
+  }
+
+  @Test()
+  @TestCase("Fetch messages")
+  public async fetchMessages() {
+    const {
+      records: { batches },
+    } = await this.chatConsumer.fetch({
+      index: 2,
+      from: OffsetFrom.End,
+    });
+
+    const events: string[] = batches
+      .map(({ records }) => {
+        return records[0].value;
+      })
+      .filter((value) => {
+        return value.includes(this.text1) || value.includes(this.text2);
+      });
+
+    expect.toBeEqual(
+      events.length,
+      2,
+      `expected two chat events, found: ${events.length}`
+    );
+  }
+}
